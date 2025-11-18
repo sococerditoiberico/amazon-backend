ff17be53b84566bb3b4f73852093f9b868e5a57f
sococerditoiberico <sococerditoiberico@gmail.com>
2025-11-18 23:49:16 +0100

    Update database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
