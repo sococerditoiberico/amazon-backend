781c793d56ba437105e367556ec3c965a03c1833
sococerditoiberico <sococerditoiberico@gmail.com>
2025-11-21 22:27:21 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,33 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from database import Base
 
-class Product(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class Product(BaseModel):
     title: str
     asin: str
     amazon_url: str
     affiliate_url: str
-    image_url: Optional[str] = None
-    price: Optional[str] = None
+    image_url: str | None
+    price: str
+
+    def to_model(self):
+        return ProductModel(
+            title=self.title,
+            asin=self.asin,
+            amazon_url=self.amazon_url,
+            affiliate_url=self.affiliate_url,
+            image_url=self.image_url,
+            price=self.price,
+        )
+
+
+class ProductModel(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    asin = Column(String)
+    amazon_url = Column(String)
+    affiliate_url = Column(String)
+    image_url = Column(String)
+    price = Column(String)
