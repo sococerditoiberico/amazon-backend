cc6666a56a2f4d69bcdd9e0b4a1d8187acbe511f
sococerditoiberico <sococerditoiberico@gmail.com>
2025-11-21 22:38:51 +0100

    Update database.py

--- database.py
+++ database.py
@@ -5,10 +5,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-from models import ProductModel
-Base.metadata.create_all(bind=engine)
