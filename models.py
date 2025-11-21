d7a0886e75e7ea19ea6461c2995720123fa7c4b2
sococerditoiberico <sococerditoiberico@gmail.com>
2025-11-22 00:20:38 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,25 +1,5 @@
-from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
 from database import Base
-
-class Product(BaseModel):
-    title: str
-    asin: str
-    amazon_url: str
-    affiliate_url: str
-    image_url: str | None
-    price: str
-
-    def to_model(self):
-        return ProductModel(
-            title=self.title,
-            asin=self.asin,
-            amazon_url=self.amazon_url,
-            affiliate_url=self.affiliate_url,
-            image_url=self.image_url,
-            price=self.price,
-        )
-
 
 class ProductModel(Base):
     __tablename__ = "products"
